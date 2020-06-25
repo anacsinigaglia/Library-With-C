ce7df1d61195bed98abff42d3534aa651cd5c80a
Ana Caroline Sinigaglia <62510719+anacsinigaglia@users.noreply.github.com>
2020-06-25 15:27:47 -0300

    Update ExResolvido1-biblioteca.c

--- ExResolvido1-biblioteca.c
+++ ExResolvido1-biblioteca.c
@@ -1,5 +1,6 @@
-/*O problema consiste em elaborar um cadastro para 20 livros, contendo as seguintes informações: código, título, autor, área, ano 
-e editora. Desenvolver um menu com as seguintes opções:
+/*O problema consiste em elaborar um cadastro para 20 livros, contendo as seguintes
+informações: código, título, autor, área, ano e editora. Desenvolver um menu com as seguintes
+opções:
 1. Cadastrar os livros.
 2. Imprimir as informações dos livros.
 3. Pesquisar livros por código.
@@ -19,7 +20,7 @@ struct livro {
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-    struct livro ficha[TAM]; //aqui estamos instanciando livro com o nome "ficha", em forma de vetor e por isso com tamanho da constante TAM
+    struct livro ficha[TAM]; /*aqui estamos instanciando livro com o nome "ficha", em forma de vetor e por isso com tamanho da constante TAM*/
     struct livro troca;
     int busca, i, j, acha, opcoes;
 
@@ -37,10 +38,50 @@ int main(){
                 scanf("%d", &ficha[i].codigo);
                 fflush(stdin);
                 
+                printf("Digite o título do livro: ");
+                scanf("%50[^\n]s", &ficha[i].titulo); /*(“%50[^\n]s”), permite que o scanf leia apenas, no máximo, 50 caracteres, ou até que a tecla ‘\n’ seja pressionada, além de permitir que sejam lidos espaços.*/
+                fflush(stdin);
                 
-    
+                printf("Digite o nome do autor: ");
+                scanf("%30[^\n]s", &ficha[i].autor);
+                fflush(stdin);
+                
+                printf("Digite a área do livro: ");
+                scanf("%30[^\n]s", &ficha[i].area);
+                fflush(stdin);
+                
+                printf("Digite o ano: ");
+                scanf("%d", &ficha[i].ano]);
+                fflush(stdin);
+                
+                printf("Digite o nome da editora: ");
+                scanf("%30[^\n]s", &ficha[i].editora);
+                fflush(stdin);
             }
         }
+        
+        else {
+        	
+        	if (opcoes == 3) {
+        		system("cls");
+        		printf("Digite o código que deseja buscar: ");
+        		scanf("%d", &busca);
+        		i = 0;
+        		acha = 0;
+        		while ((i < TAM) && <acha == 0) {
+        			if (ficha[i].codigo == busca) {
+        				acha = 1;
+					} else {
+						i++;
+					}
+					
+					if (acha == 1) {
+						printf("\nCÓDIGO: %d\n", ficha[i].codigo);
+						printf("\nTÍTULO: %s\n", ficha[i].titulo);
+						
+					}
+				}
+			}
+		}
     }
 }
-
